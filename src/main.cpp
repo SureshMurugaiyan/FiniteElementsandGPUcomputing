cb1f7d82afbcd2229090a6ce31f48689ad231bf5
SureshMurugaiyan <37843174+SureshMurugaiyan@users.noreply.github.com>
2018-10-11 15:03:55 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,12 @@ int main(int argc, char **argv)
 {
 //USER INPUT
 // Opposite boundaries should have equal number of points
-const int nxc = 6;      // number of cells in north and south boundary
-const int nyc = 6;      // number of cells in east and west boundary
+const int nxc = 64;      // number of cells in north and south boundary
+const int nyc = 64;      // number of cells in east and west boundary
 double Re = 100.0;         // User Input instead of material property
 double Co = 0.4; 	// Courant number
 
-int noIteration=2;
+int noIteration=13000;
 
 
 const int nx  = nxc+1; // number of grid points in x-direction
@@ -42,26 +42,13 @@ const int nVfc  = nxVfc*nyVfc;
 double X[nx*ny];
 double Y[nx*ny];
 double Z[nx*ny];
-double XC[(ny-1)*(nx-1)];
-double YC[(ny-1)*(nx-1)];
-double ZC[(ny-1)*(nx-1)];
 
-
-double X_HFC[(ny)*(nx-1)];
-double Y_HFC[(ny)*(nx-1)];
-double Z_HFC[(ny)*(nx-1)];
-
-
-double X_VFC[(ny-1)*(nx)];
-double Y_VFC[(ny-1)*(nx)];
-double Z_VFC[(ny-1)*(nx)];
-
-
-mesh2Dsquare(X,Y,Z,XC,YC,ZC,X_HFC,Y_HFC,Z_HFC,X_VFC,Y_VFC,Z_VFC,nxc,nyc,nx,ny,nc);
+mesh2Dsquare(X,Y,Z,nxc,nyc,nx,ny,nc);
 double dx = 1.0/nxc;
 double dy = 1.0/nyc;
+double dz = dx;
 
-double dt = 0.00001; //Co*dx;
+double dt = 0.003; //Co*dx;
 double dV = dx*dy;
 
 double ux[nc]; // x component of velocity
@@ -69,10 +56,21 @@ double uy[nc]; // y component of velocity
 double uz[nc]; // z component of velocity
 double p[nc];  // Pressure
 
+
+double uxOld[nc]; // x component of velocity
+double uyOld[nc]; // y component of velocity
+double uzOld[nc]; // z component of velocity
+double pOld[nc];  // Pressure
+
 double Dnx[nc]; // allocating space for Diffusion term
 double Dny[nc]; // allocating space for Diffusion term
 double Cnx[nc]; // allocating space for Convection term
 double Cny[nc]; // allocating space for Convection term
+
+double DnxOld[nc]; // allocating space for Diffusion term
+double DnyOld[nc]; // allocating space for Diffusion term
+double CnxOld[nc]; // allocating space for Convection term
+double CnyOld[nc]; // allocating space for Convection term
 
 double Cn[nc]; //  allocating space for Convection source term in pressure poisson
 double gradxP[nc]; //  allocating space for gradient
@@ -82,9 +80,140 @@ double vertP[ny*nx];
 double hfaceP[nHfc]; //  allocating space for gradient
 double vfaceP[nVfc]; //  allocating space for gradient
 boundaryCondition(ux,uy,p,nxc,nyc,nc);
+//L2-norm
+double L2ux = 1.0;
+double L2uy = 1.0;
+double L2uz = 1.0;
+double L2p  = 1.0;
+
+//L2-norm
+double L2oux = 1.0;
+double L2ouy = 1.0;
+double L2ouz = 1.0;
+double L2op  = 1.0;
+
+//--------------- Predictor step--------------------------------//
+ Laplacian(Dnx,ux,nyc,nxc,dx,dy);      //  Dnx Diffusion term
+ Laplacian(Dny,uy,nyc,nxc,dx,dy);      //  Dny Diffusion term
+ Div(Cnx,ux,ux,uy,nyc,nxc,dx,dy);      //  Cnx Convection term
+ Div(Cny,uy,ux,uy,nyc,nxc,dx,dy);      //  Cny Convection term
+
+
+// Parallelizing
+// Initialize MPI: always do this "early"
+MPI_Init(&argc, &argv);
+int rank;
+int size;
+
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+MPI_Comm_size(MPI_COMM_WORLD, &size);
+printf("This is process %d of %d total processes\n",rank,size);
+
+// collective barrier
+MPI_Barrier(MPI_COMM_WORLD);  // all processor should enter this barrier function // synchrony
+
+// send a "message" from rank 0 to rank 1
+  if(rank==0){
+    int dimension = 3;      // size of message out
+     double *dxdydzout = (double*) 
+      calloc(dimension, sizeof(double));
+
+    
+      dxdydzout[0] = dx;
+      dxdydzout[1] = dy;
+      dxdydzout[2] = dz;
+
+    int dest = 1;
+    int tag = 666;
+    
+    MPI_Request sendRequest;
+    
+    MPI_Isend(dxdydzout, 
+	      dimension,
+	      MPI_DOUBLE,
+	      dest,
+	      tag,
+	      MPI_COMM_WORLD,
+	      &sendRequest);
+
+    // random task
+    int k = 87;
+    for(int j=0;j<10000;++j){
+      k = (k^j)*3;
+    }
+    
+    MPI_Status status;
+    MPI_Wait(&sendRequest, &status);
+
+    free(dxdydzout);
+
+  }
+
+  // recv a "message" from rank 1
+  if(rank==1){
+    int dimension = 3; 
+double *dxdydzin = (double*) 
+      calloc(dimension, sizeof(double));
+
+
+    int source = 0;
+    int tag = 666;
+    
+    MPI_Request recvRequest;
+    
+    MPI_Irecv(dxdydzin, 
+	      dimension,
+	      MPI_DOUBLE,
+	      source,
+	      tag,
+	      MPI_COMM_WORLD,
+	      &recvRequest);
+    
+    // random task
+    int k = 99;
+    for(int j=0;j<1000;++j){
+      k = (k^j)*3;
+    }
+    
+    MPI_Status status;
+    MPI_Wait(&recvRequest, &status);
+
+double dx = dxdydzin[0];
+double dy = dxdydzin[1];
+double dz = dxdydzin[2];
+cout<<dx<<endl;
+
+    for(int i=0;i<dimension;++i){
+      printf("dxdydzin[%d] = %4.8f\n",
+	     i, dxdydzin[i]);
+   }
+    
+   free(dxdydzin);
+
+  }
+
+  // tear down MPI (potentially block for all proceses)
+  MPI_Finalize();
+
+
+
+
+/*
 
 int iteration = 1;
 while (iteration < noIteration){
+//--------------Store old values-------------------//
+storeOldValue(ux,uxOld,nc);
+storeOldValue(uy,uyOld,nc);
+storeOldValue(uz,uzOld,nc);
+storeOldValue(p,pOld,nc);
+
+storeOldValue(Dnx,DnxOld,nc);
+storeOldValue(Dny,DnyOld,nc);
+storeOldValue(Cnx,CnxOld,nc);
+storeOldValue(Cny,CnyOld,nc);
+
+
 
 //--------------- Predictor step--------------------------------//
  Laplacian(Dnx,ux,nyc,nxc,dx,dy);      //  Dnx Diffusion term
@@ -94,13 +223,14 @@ while (iteration < noIteration){
 
 
  eulerPredictor(ux,uy,nxc,nyc,dt,dV,Cnx,Cny,Dnx,Dny,Re);
+//adamPredictor(ux,uy,nxc,nyc,dt,dV,Cnx,Cny,Dnx,Dny,CnxOld,CnyOld,DnxOld,DnyOld,Re);
 
 //--------------Solve poisson eqn for Pressure ---------------//
 Divergence(Cn,ux,uy,nyc,nxc,dx,dy); //  source term in poisson equation
 
 updateCn(Cn,dt,nxc,nyc);
 
-Poisson(p,nyc,nxc,dx,dy,Cn);
+Poisson(p,nyc,nxc,dx,dy,Cn,nc);
 
 //--------------- Corrector Step ----------------------
 
@@ -115,129 +245,26 @@ updateBoundaryCondition(ux,uy,p,nxc,nyc,nc);
 //--------------- Update reference Pressure ----------------------/
 
 refPressure(p,nyc, nxc);
-//cout<<iteration<<endl;
+if(iteration==1){
+L2norm(ux,uxOld, &L2oux,nc);
+L2norm(uy,uyOld, &L2ouy,nc);
+L2norm(uz,uzOld, &L2ouz,nc);
+L2norm(p,pOld, &L2op,nc);
+}
+
+L2norm(ux,uxOld, &L2ux,nc);
+L2norm(uy,uyOld, &L2uy,nc);
+L2norm(uz,uzOld, &L2uz,nc);
+L2norm(p,pOld, &L2p,nc);
+
+cout<<endl<<"iteration no:"<<iteration<<"\t"<<"u residual:"<<L2ux/L2oux<<"\t"
+     <<"v residual:"<<L2uy/L2ouy<<"\t"<<"P residual:"<<L2p/L2op<<"\t"<<endl;
 iteration = iteration+1;
-
 }
 
-//printData(ux,uy,p,nxc,nyc);
+printData(ux,uy,p,nxc,nyc);
+*/
 
-// Parallelizing
-// Initialize MPI: always do this "early"
-MPI_Init(&argc, &argv);
-
-int rank;
-int size;
-
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-printf("This is process %d of %d total processes\n",rank,size);
-
-// collective barrier
-MPI_Barrier(MPI_COMM_WORLD);  // all processor should enter this barrier function // synchrony
-//if(rank==0) printf("hello from bob\n");
-
-// send a "message" from rank 0 to rank1
-
-int ncL = (nc)/2;
-
-if(rank==0){
-  int messageN=ncL;
-  int *messageOutXL = (int*)
-    calloc(messageN, sizeof(int));
-  for(int i=ncL+1; i<messageN;++i){
-    XL[i] = X[i];
-    YL[i] = Y[i];
-    ZL[i] = Z[i];
-    XCL[i] = XC[i];
-    YCL[i] = YC[i];
-    ZCL[i] = ZC[i];
-    X_HFCL[i] = X_HFC[i];
-    Y_HFCL[i] = Y_HFC[i];
-    Z_HFCL[i] = Z_HFC[i];
-    X_VFCL[i] = X_VFC[i];
-    Y_VFCL[i] = Y_VFC[i];
-    Z_VFCL[i] = Z_VFC[i];
-  }
-  int dest = 1;
-  int tagX = 661;  // for vector X
-  int tagY = 662;  // for vector Y
-  int tagZ = 663;  // for vector Y
-  int tagXC = 664; // for vector Y
-  int tagYC = 665;  // for vector Y
-  int tagZC = 666;  // for vector Y
-  int tagX_HFC = 667; // for vector Y
-  int tagY_HFC = 668;  // for vector Y
-  int tagZ_HFC = 669;  // for vector Y
-  int tagX_VFC = 670; // for vector Y
-  int tagY_VFC = 671;  // for vector Y
-  int tagZ_VFC = 672;  // for vector Y
-  MPI_Request sendRequest;
-  MPI_Isend(messageOutXL, messageN,MPI_INT,dest,tagX,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutYL, messageN,MPI_INT,dest,tagY,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutZL, messageN,MPI_INT,dest,tagY,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutXCL, messageN,MPI_INT,dest,tagXC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutYCL, messageN,MPI_INT,dest,tagYC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutZCL, messageN,MPI_INT,dest,tagZC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutX_HFCL, messageN,MPI_INT,dest,tagX_HFC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutY_HFCL, messageN,MPI_INT,dest,tagY_HFC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutZ_HFCL, messageN,MPI_INT,dest,tagZ_HFC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutX_VFCL, messageN,MPI_INT,dest,tagX_VFC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutY_VFCL, messageN,MPI_INT,dest,tagY_VFC,MPI_COMM_WORLD,&sendRequest);
-  MPI_Isend(messageOutZ_VFCL, messageN,MPI_INT,dest,tagZ_VFC,MPI_COMM_WORLD,&sendRequest);
-
-// random task
-int k = 87;
-for(int j=0;j<10000;++j){
-k = (k^j)*3;
-}
-
-MPI_Status status;
-MPI_Wait(&sendRequest, &status);
-
-  free(messageOutXL);  // blocking communication routine
-}
-
-
-// rank 1 receives a "message" from rank 0
-
-if(rank==1){
-  int tag =666;
-  int messageN=ncL;
-  int *messageIn = (int*)
-    calloc(messageN, sizeof(int));
-  int source=0;
-  MPI_Request recvRequest;
-
-  MPI_Irecv(messageIn,
-            messageN,
-            MPI_INT,
-            source,
-            tag,
-            MPI_COMM_WORLD,
-            &recvRequest);
-
-
-// random task
-int k = 99;
-for(int j=0;j<1000;++j){
-k = (k^j)*3;
-}
-
-MPI_Status status;
-MPI_Wait(&recvRequest, &status);
-  for(int i=0; i<messageN;++i){
-    printf("messageIn[%d]=%d\n",
-           i,messageIn[i]);
-  }
-  free(messageIn);  // blocking communication routine
-}
-
-
-
-// tear down MPI( potentially block for all processes)
-MPI_Finalize(); // Book keeping. All of the instances to wait
 return 0;
 //END OF PROGRAM
 
