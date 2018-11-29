cef54eced5f164d880004e73e4f0da3c46be0090
SURESH MURUGAIYAN <37843174+SureshMurugaiyan@users.noreply.github.com>
2018-11-29 11:58:02 -0500

    Add files via upload

--- include/input.h
+++ include/input.h
@@ -1,10 +1,10 @@
 //USER INPUT
 #define Mode 'P'       // Choice S == Serial Mode and Choice P == Parallel mode
-#define nxc 64         // Number of cells in North and South Boundary
-#define nyc 64         // Number of cells in East  and West  Boundary
+#define nxc 32         // Number of cells in North and South Boundary
+#define nyc 32         // Number of cells in East  and West  Boundary
 #define Re  100.0      // Number of cells in East  and West  Boundary
 #define pRefCell 1     // Pressure Reference cell // Assume center of cell
 //USER INPUT FOR PARALLEL
-#define nxcL 32  // number of cells in north and south of each chunk --- chunknxc
-#define nycL 32  // number of cells in each and west boundary of each chunk------chunknyc
+#define nxcL 16  // number of cells in north and south of each chunk --- chunknxc
+#define nycL 16  // number of cells in each and west boundary of each chunk------chunknyc
 
